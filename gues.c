557c78d421ac52927205ecafaf1ed3336059a614
Omar925-afk <140343627+Omar925-afk@users.noreply.github.com>
2026-09-06 18:03:19 +0000

    Sun, Sep 6, 2026, 9:03 PM +03:00

--- gues.c
+++ gues.c
@@ -1,40 +1,35 @@
+#include <cs50.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 int main(void)
 {
-    // 1. تهيئة الرقم العشوائي
+    // تهيئة الرقم العشوائي
     srand(time(NULL));
     int secret_number = (rand() % 100) + 1;
-    int guess;
+    int guess = 0;
     int attempts = 0;
 
-    printf("--- اهلا بك في لعبة تخمين الرقم ---\n");
-    printf("لقد اخترت رقماً بين 1 و 100. هل يمكنك معرفته؟\n\n");
+    printf("--- لعبة تخمين الرقم (نسخة CS50) ---\n");
 
-    // 2. حلقة التكرار: تستمر طالما أن التخمين لا يساوي الرقم السري
-    do
+    // استخدام while loop مع get_int من مكتبة cs50
+    while (guess != secret_number)
     {
-        printf("أدخل تخمينك: ");
-        scanf("%i", &guess);
-        attempts++; // زيادة عدد المحاولات في كل مرة
+        guess = get_int("أدخل تخمينك: ");
+        attempts++;
 
         if (guess > secret_number)
         {
-            printf("خطأ! الرقم السري (أصغر) من ذلك. حاول مرة أخرى.\n");
+            printf("أصغر! \n");
         }
         else if (guess < secret_number)
         {
-            printf("خطأ! الرقم السري (أكبر) من ذلك. حاول مرة أخرى.\n");
+            printf("أكبر! \n");
         }
         else
         {
-            printf("\nمبروك! لقد حزرت الرقم الصحيح وهو %i\n", secret_number);
-            printf("لقد استغرقت %i محاولات للفوز.\n", attempts);
+            printf("مبروك! الرقم هو %i وعدد محاولاتك %i\n", secret_number, attempts);
         }
-
-    } while (guess != secret_number);
-
-    return 0;
+    }
 }
