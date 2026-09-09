ee25db0adf49c195690c6a27f79edebdef14fa35
Omar925-afk <140343627+Omar925-afk@users.noreply.github.com>
2026-09-09 20:16:09 +0000

    Wed, Sep 9, 2026, 11:16 PM +03:00

--- avg.c
+++ avg.c
@@ -1,34 +1,10 @@
- #include <stdio.h>
- #include <cs50.h>
-
+#include <stdio.h>
+#include <cs50.h>
  int main(void)
- {
-	 int n = get_int("How many numbers? ");
-	 int numbers[n];
-
-	 for (int i = 0; i < n; i++)
-	 {
-		 numbers[i] = get_int("Enter number: ");
-	 }
-
-	 int target = get_int("Search for: ");
-	 int found_index = -1;
-
-	 for (int i = 0; i < n; i++)
-	 {
-		 if (numbers[i] == target)
-		 {
-			 found_index = i;
-			 break;
-		 }
-	 }
-
-	 if (found_index == -1)
-	 {
-		 printf("not found\n");
-	 }
-	 else
-	 {
-		 printf("Found at index %i\n", found_index);
-	 }
- }
+{
+    int n = get_int("Enter a number: "); // طلب من المستخدم إدخال رقم
+    int num = get_int("Enter another number: "); // طلب من المستخدم إدخال رقم آخر
+    int sum = n + num; // جمع الرقمين وتخزين النتيجة في متغير sum
+    printf("The sum of %d and %d is: %d\n", n, num, sum); // عرض النتيجة للمستخدم
+    return 0;
+}
