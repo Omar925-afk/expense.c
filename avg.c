eb2f95ef1ea617a3fda3865647d7264f613e8343
Omar925-afk <140343627+Omar925-afk@users.noreply.github.com>
2026-09-09 20:07:48 +0000

    Wed, Sep 9, 2026, 11:07 PM +03:00

--- avg.c
+++ avg.c
@@ -7,14 +7,14 @@ int main (void)
  int n = get_int("enter number : ");
 
 // مصفوفه ARRAYS
+     
 
+    int number = 3;
 
-    int number = 0;
-
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < 3; i++)
     {
 
-         scanf("%d", &number); 
+         scanf("%d", &number);
 
     }
 
