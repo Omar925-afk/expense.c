2050bf57cab16dc2b43c27b288986bfb9fbe91ae
Omar925-afk <140343627+Omar925-afk@users.noreply.github.com>
2026-09-09 20:21:09 +0000

    Wed, Sep 9, 2026, 11:21 PM +03:00

--- avg.c
+++ avg.c
@@ -2,9 +2,8 @@
 #include <cs50.h>
  int main(void)
 {
-    int n = get_int("Enter a number: "); // طلب من المستخدم إدخال رقم
-    int num = get_int("Enter another number: "); // طلب من المستخدم إدخال رقم آخر
-    int sum = n + num; // جمع الرقمين وتخزين النتيجة في متغير sum
-    printf("The sum of %d and %d is: %d\n", n, num, sum); // عرض النتيجة للمستخدم
-    return 0;
-}
+    int x = get_int("Enter a number: "); // طلب من المستخدم إدخال رقم
+    int y = get_int("Enter a number: "); // طلب من المستخدم إدخال رقم
+    printf( " %i\n", x + y); // عرض مجموع الرقمين
+
+
